148c19fe9f3164d4798f593634b6782c62c8dcbc
kenselasie <kenselasie@gmail.com>
2022-03-18 13:49:02 +0000

    Update: Implemented UseFetch Hook Logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState, useCallback } from 'react';
 import './App.css';
+import { useFetch } from './hooks/useFetch'
 
 function App() {
+  const url1 = 'https://jsonplaceholder.typicode.com/users/1'
+  const url2 = 'https://jsonplaceholder.typicode.com/users/5'
+  const [name, setName] = useState('')
+  const {data, error, loading, setUrl, setData} = useFetch(url1, (response) => {
+    console.log('Successfully called callback function', response)
+  })
+ 
+  const setNewUrl = () => {
+    setUrl(() => url2)
+  }
+  
+
+  const setNewData = useCallback(() => {
+    if (error) return
+    if (!name) return
+    setData((prevData) => { return {...prevData, name: name} })
+    // return setName(() => '')
+  }, [name])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {loading && <h1>Loading..</h1> }
+      {error && <h1>An error occured</h1> }
+      { data && 
+        <div>
+          <h1>User Profile</h1>
+          <p>Name: {data.name}</p>
+          <p>Email: {data.email}</p>
+          <button onClick={setNewUrl}>Set User Two</button>
+          <div style={{margin: '40px'}}>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+            <button onClick={setNewData}>Update Name</button>
+          </div>
+        </div>}
     </div>
   );
 }
